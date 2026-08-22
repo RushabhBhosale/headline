8f393f36ef322b8c0bf75b88dea09fdb19de703d
Rushabh <145191870+RushabhBhosale@users.noreply.github.com>
2026-08-22 23:31:29 +0530

    sanity schema added

--- studio/headline/schemaTypes/index.ts
+++ studio/headline/schemaTypes/index.ts
@@ -1,1 +1,14 @@
-export const schemaTypes = []
+import { article } from "../../../sanity/schemas/article";
+import { author, category, siteSettings, topic } from "../../../sanity/schemas/base";
+import { homepage } from "../../../sanity/schemas/homepage";
+import { storyThread } from "../../../sanity/schemas/storyThread";
+
+export const schemaTypes = [
+  siteSettings,
+  homepage,
+  article,
+  storyThread,
+  author,
+  category,
+  topic,
+];
